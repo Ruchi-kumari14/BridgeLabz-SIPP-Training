7118491d25841b86250e31cdb599891a2bbb7550
ruchi <122520470+Ruchi-kumari14@users.noreply.github.com>
2025-06-24 09:36:27 +0530

    Delete JAVAfundamentals directory

--- JAVAfundamentals/HarrysAge.java
+++
@@ -1,8 +0,0 @@
-class HarrysAge{
-    public static void main(String[] args){
-       int Birthyear=2000;
-       int Currentyear=2024;
-       int age=Currentyear-Birthyear;
-       System.out.printf("Harry's age in " + Currentyear + "is" + age);
-    }
-}
